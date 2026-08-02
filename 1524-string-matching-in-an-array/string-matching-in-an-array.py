c696565041d5b8578b9aab465cbbea07752a44b3
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-02 19:47:04 +0530

    Time: 4 ms (54.70%) | Memory: 12.3 MB (90.61%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def stringMatching(self, words):
+        hook=[]
+        for i in range(len(words)):
+            condition=False
+            for j in range(len(words)):
+                if words[i] in words[j] and len(words[i])<len(words[j]):
+                    hook.append(words[i])
+                    break
+                
+            
+        return hook
+
+
+        
