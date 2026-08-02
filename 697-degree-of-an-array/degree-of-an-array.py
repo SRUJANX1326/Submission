72d9cf35ff33f2241e1b80ce951b5b80daf0d2ce
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-02 17:47:55 +0530

    Time: 3498 ms (5.26%) | Memory: 13.1 MB (75.79%) - LeetSync

---
+++ 697-degree-of-an-array/degree-of-an-array.py
@@ -0,0 +1,57 @@
+import math
+class Solution(object):
+    def findShortestSubArray(self, nums):
+        items=[]
+        dup=[]
+        degree=nums.count(nums[0])
+        nums_set=set(nums)
+        nums_list=list(nums_set)
+        for i in range(len(nums_list)):
+            if nums.count(nums_list[i])>=degree:
+                if nums_list[i] in items:
+                    pass
+                elif nums_list[i] not in items and nums.count(nums_list[i])==degree:
+                    items.append(nums_list[i])
+                elif nums_list[i] not in items and nums.count(nums_list[i])>=degree:
+                    items=dup[:]
+                    items.append(nums_list[i])
+                    degree=nums.count(nums_list[i])
+            print(i)
+        min_degree=float('inf')
+        if len(items)>1:
+            while len(items)>0:
+                i=0
+                j=len(nums)-1
+                while True:
+                    if nums[i]!=items[0]:
+                        i+=1
+                    else:
+                        break
+                while True:
+                    if nums[j]!=items[0]:
+                        j-=1
+                    else:
+                        break
+                items.remove(items[0])
+                temp=j-i+1
+                if min_degree>temp:
+                    min_degree=temp  
+
+                print(temp,min_degree)            
+        else:
+            i=0
+            j=len(nums)-1
+            while True:
+                if nums[i]!=items[0]:
+                    i+=1
+                else:
+                    break
+            while True:
+                if nums[j]!=items[0]:
+                    j-=1
+                else:
+                    break
+            min_degree=j-i+1
+        return min_degree
+
+        
