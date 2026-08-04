a4de822feb24d1547f2333530864e6ccf41e15b0
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-04 12:21:29 +0530

    Time: 11 ms (22.81%) | Memory: 12.4 MB (17.28%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -2,7 +2,7 @@ class Solution(object):
     def findMissingElements(self, nums):
         res=[]
         if len(nums)!=(max(nums)-min(nums)+1):
-            for i in range(min(nums),max(nums)+1):
+            for i in range(min(nums)+1,max(nums)):
                 if i not in nums:
                     res.append(i)
             return res
