47091d62ceb3a4e306aed9ae454059d51bcabda1
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-04 12:19:50 +0530

    Time: 10 ms (26.50%) | Memory: 12.4 MB (17.28%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -6,9 +6,7 @@ class Solution(object):
         print(r-l)
         if len(nums)!=(r-l+1):
             for i in range(l,r+1):
-                print(i)
                 if i not in nums:
-                    print(i)
                     res.append(i)
             return res
         else:
