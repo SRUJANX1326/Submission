5220d92904eb42adcde90621613b938591e637c6
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-05 09:06:04 +0530

    Time: 3 ms (18.92%) | Memory: 12.3 MB (62.16%) - LeetSync

---
+++ 1760-check-array-formation-through-concatenation/check-array-formation-through-concatenation.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canFormArray(self, arr, pieces):
+        res=[]
+        for i in range(len(arr)):
+            for j in range(len(pieces)):
+                if arr[i]==pieces[j][0]:
+                    res=res+pieces[j]
+                    break
+            if res[:i]!=arr[:i]:
+                return False
+        if len(arr)==len(res) and arr==res:
+            return True
+        else:
+            return False
+        
