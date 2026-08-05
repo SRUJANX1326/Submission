d3ea655f854518fe17a0cd30f0ced4b353030338
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-05 08:41:55 +0530

    Time: 18 ms (11.86%) | Memory: 12.4 MB (23.73%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -8,7 +8,6 @@ class Solution(object):
             else:
                 help_dict[nums.count(x)].add(x)
         for dummy in range(len(nums)):
-            print(dummy)
             if list(help_dict)==[]:
                 continue
             freq=min(help_dict)
@@ -17,9 +16,7 @@ class Solution(object):
                 continue
             ele=max(help_dict[freq])
             help_dict[freq].discard(ele)
-            print(freq,ele)
             for i in range(freq):
-                print(i)
                 res.append(ele)
         return res
 
