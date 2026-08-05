1a8e2a22c2820fc147c679f64a0aee343bfd54de
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-05 10:41:37 +0530

    Time: 359 ms (1.33%) | Memory: 12.2 MB (100.00%) - LeetSync

---
+++ 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def getMaximumGenerated(self, n):
+        if n==0:
+            return 0
+        nums=[0,1]
+        for i in range(2,n+1):
+            nums.append(0)
+        condition=True
+        for i in range(0,(n+1)//2):
+            print(nums,i)
+            temp=2 * i
+            nums[temp]=nums[i]
+            nums[temp+1]=nums[i] + nums[i + 1]
+        return max(nums)
+
+        
