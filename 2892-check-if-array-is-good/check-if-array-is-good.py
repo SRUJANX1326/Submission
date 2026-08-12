ee3292d9d775ac549a491b7f45ec3a465e9b97fd
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-12 13:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.09%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def isGood(self, nums):
         nums.sort()
-        if nums==[1,1]:
-            return True
         if nums[-1]!=len(nums)-1:
             return False
         for i in range(len(nums)-1):
