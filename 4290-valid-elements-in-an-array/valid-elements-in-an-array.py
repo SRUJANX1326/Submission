5977172ef1098373dd24d26fec4f790063b22442
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-12 17:16:41 +0530

    Time: 11 ms (52.75%) | Memory: 12.4 MB (59.34%) - LeetSync

---
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def left_answer_function(self,arr):
+        for x in arr[1:]:
+            if arr[0]<=x:
+                return False
+        return True
+    def right_answer_function(self,arr):
+        for x in arr[:-1]:
+            if arr[-1]<=x:
+                return False
+        return True
+
+
+    def findValidElements(self, nums):
+        result=[]
+        result.append(nums[0])
+        for i in range(1,len(nums)-1):
+            if self.left_answer_function(nums[i:]) or self.right_answer_function(nums[:i+1]):
+                result.append(nums[i])
+        if len(nums)>1:
+            result.append(nums[-1])
+        return result
+        
