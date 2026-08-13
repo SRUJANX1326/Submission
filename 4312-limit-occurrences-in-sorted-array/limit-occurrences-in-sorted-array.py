acdcc0ac446329c834e9feb860257c01a1a20481
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-13 08:16:41 +0530

    Time: 50 ms (5.19%) | Memory: 12.4 MB (22.08%) - LeetSync

---
+++ 4312-limit-occurrences-in-sorted-array/limit-occurrences-in-sorted-array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def limitOccurrences(self, nums, k):
+        freq=dict()
+        set_nums=set(nums)
+        for x in set_nums:
+            freq[x]=nums.count(x)
+        res=[]
+        for x in set_nums:
+            lim=0
+            if freq[x]<=k:
+                lim=freq[x]
+            else:
+                lim=k
+            for i in range(lim):
+                res.append(x)
+        print(res)
+        res.sort()
+        return res
+                
+
+
+        
