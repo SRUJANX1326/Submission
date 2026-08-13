1f494f4e2df80152e1ef12095a975aeeb31d82bd
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-13 13:18:48 +0530

    Time: 1 ms (43.43%) | Memory: 12.4 MB (16.16%) - LeetSync

---
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numberOfPairs(self, nums):
+        freq=dict()
+        for x in nums:
+            freq[x]=nums.count(x)
+        count_pair=0
+        count_remaining=0
+        for x in freq:
+            if freq[x]>=2 and freq[x]%2==0:
+                count_pair+=freq[x]/2
+            elif freq[x]>2 and freq[x]%2!=0:
+                count_pair+=(freq[x]-1)/2
+                count_remaining+=1
+            else:
+                count_remaining+=1
+        return [count_pair,count_remaining]
+
+
+        
