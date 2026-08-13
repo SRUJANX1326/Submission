2ab088b6cb8dce886e82cc4ec7f0b16d3ad9c472
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-13 11:49:53 +0530

    Time: 1 ms (43.89%) | Memory: 12.3 MB (59.13%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -1,11 +1,6 @@
 class Solution(object):
     def targetIndices(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i]<nums[j]:
-                    temp=nums[i]
-                    nums[i]=nums[j]
-                    nums[j]=temp
+        nums.sort()
         res=[]
         for i in range(len(nums)):
             if nums[i]==target:
