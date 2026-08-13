ded67df2f6f2c37d1855309877417976b64a4baf
SRUJANX1326 <srujanx1326@gmail.com>
2026-08-13 13:19:31 +0530

    Time: 1 ms (43.43%) | Memory: 12.3 MB (90.91%) - LeetSync

--- 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.py
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.py
@@ -3,8 +3,7 @@ class Solution(object):
         freq=dict()
         for x in nums:
             freq[x]=nums.count(x)
-        count_pair=0
-        count_remaining=0
+        count_pair,count_remaining=(0,0)
         for x in freq:
             if freq[x]>=2 and freq[x]%2==0:
                 count_pair+=freq[x]/2
