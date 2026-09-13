ccd4f0ab210fe384db15e54f222ca9a129e215fc
SRUJANX1326 <srujanx1326@gmail.com>
2026-09-14 00:08:04 +0530

    Time: 1 ms (13.24%) | Memory: 42.6 MB (80.03%) - LeetSync

---
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeSet<Integer> ans=new TreeSet();
+    public void traverse(TreeNode root){
+        if(root==null) return;
+        ans.add(root.val);
+        traverse(root.left);
+        traverse(root.right);
+    }
+    public int findSecondMinimumValue(TreeNode root) {
+        traverse(root);
+        ans.removeFirst();
+        int ssm;
+        try{
+            ssm=ans.getFirst();
+        }catch(Exception E){
+            return -1;
+        }
+        return ssm;
+    }
+}
