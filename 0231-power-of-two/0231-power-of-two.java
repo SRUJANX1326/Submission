92585e908a8aa3b427f979ff3dc6f34a4a43ec3c
SRUJANX1326 <srujanx1326@gmail.com>
2026-09-19 09:17:09 -0700

    Time: 1 ms (96.62%), Space: 42.6 MB (55.44%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0) return false;
-        int max=1073741824;
-        if(max%n==0) return true;
+        if(1073741824%n==0) return true;
         return false;
     }
 }
