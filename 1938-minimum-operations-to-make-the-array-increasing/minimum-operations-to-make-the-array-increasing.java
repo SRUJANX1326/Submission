b5c391b638bc4d82bfcf91321b44e5e57a698054
SRUJANX1326 <srujanx1326@gmail.com>
2026-09-26 08:05:18 -0700

    Time: 4 ms (71.76%) | Memory: 47.2 MB (7.64%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -3,7 +3,7 @@ class Solution {
         int count=0;
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]>=nums[i+1]){
-                int increment=Math.abs(nums[i]-nums[i+1]);
+                int increment=nums[i]-nums[i+1];
                 nums[i+1]+=increment+1;
                 count+=increment+1;
             }
